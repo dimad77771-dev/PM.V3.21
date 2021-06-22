9f5d124a0dfbc17c5f82ddb2cec0378990d67af2
Profibiz <profibiz2014@gmail.com>
2021-06-22 11:37:18 -0400

    Added correct labels

--- Profibiz.PracticeManager.Shell/Properties/AssemblyInfo.cs
+++ Profibiz.PracticeManager.Shell/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Profibiz.PracticeManager.Shell")]
+[assembly: AssemblyTitle("Profibiz PracticeManager 2021")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Profibiz.PracticeManager.Shell")]
+[assembly: AssemblyProduct("Profibiz PracticeManager 2021")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -51,5 +51,6 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("3.1.0.1")]
+[assembly: AssemblyFileVersion("3.1.0.1")]
+[assembly: NeutralResourcesLanguage("")]
